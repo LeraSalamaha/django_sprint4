bc47e05c96aea710c12ed86a1761e0bfdef1bb14
Валерия Саламаха <lera_salamakha@vk.com>
2024-12-05 19:02:45 +0300

    final

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -7,9 +7,3 @@ def about(request):
 
 def rules(request):
     return render(request, 'pages/rules.html')
-
-
-'''def page_not_found(request, exception):
-    # Переменная exception содержит отладочную информацию; 
-    # выводить её в шаблон пользовательской страницы 404 мы не станем.
-    return render(request, 'pages/404.html', status=404)'''
--- blogicum/users/views.py
+++ blogicum/users/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
 def registration(request):
     pass
+
 
 @login_required
 def profile(request):
